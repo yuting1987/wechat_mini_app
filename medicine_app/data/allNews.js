e013e51e9ddf0baa4ec82c164c1cb6595bcaa6ca
yuting <yuting1987000@163.com>
2017-01-16 14:38:16 +0800

    tijiao

--- medicine_app/data/allNews.js
+++ medicine_app/data/allNews.js
@@ -1,13 +1,15 @@
 module.exports = [
     {
-        id : 1,
-        imgUrl : 'http://image.tycredit.com/2/7560c0c50d40.png',
-        title : '测试测试',
-        content : '测试测试哈哈哈哈哈哈'
-    },{
         id : 2,
-        imgUrl : 'http://image.tycredit.com/2/7560c0c50d40.png',
+        imgUrl : 'http://192.168.131.58:8080/picture/banner3.jpg',
         title : '测试测试',
-        content : '测试测试哈哈哈哈哈哈'
+        content : '测试测试哈哈哈哈哈哈',
+        date : '2017-01-02'
+    },{
+        id : 1,
+        imgUrl : 'http://192.168.131.58:8080/picture/banner2.jpg',
+        title : '测试测试',
+        content : '测试测试哈哈哈哈哈哈',
+        date : '2017-01-01'
     }
 ]
